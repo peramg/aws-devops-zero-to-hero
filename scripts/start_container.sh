57a8a568aa787d33d45abede0a6a8ddd38d3dc50
peramg <gnaneshperam985@gmail.com>
2025-08-04 09:51:53 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull peramg1/simple-python-flask-app
+docker pull peramg1/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 peramg1/simple-python-flask-app
+docker run -d -p 5000:5000 peramg1/simple-python-app
